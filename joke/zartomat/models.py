ec5a4d839336fd6de5ab6b5b7cb228627a809877
Mheeh <dreekus@gmail.com>
2014-06-09 09:37:55 +0200

    Model Joke

--- joke/zartomat/models.py
+++ joke/zartomat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Joke(models.Model):
+    joke_text = models.CharField('joke_text', max_length=200)
+    published_date = models.DateTimeField('date published')
+    accepted = models.BooleanField('accepted')
+    rate = models.IntegerField('rate')
+    number_of_grades = models.IntegerField('number_of_grades')
