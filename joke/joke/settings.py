b66c5674ef9c0532a09b619fbcc0da56bfce0a83
m87 <m87blackhole@gmail.com>
2014-06-09 23:36:10 +0200

    users model

--- joke/joke/settings.py
+++ joke/joke/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-)
+    'zartomat',
+    )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
--- joke/zartomat/models.py
+++ joke/zartomat/models.py
@@ -3,7 +3,16 @@ from django.db import models
 # Create your models here.
 class Joke(models.Model):
     joke_text = models.CharField('joke_text', max_length=200)
-    published_date = models.DateTimeField('date published')
+    published_date = models.DateTimeField('date_published')
     accepted = models.BooleanField('accepted')
     rate = models.IntegerField('rate')
     number_of_grades = models.IntegerField('number_of_grades')
+
+class User(models.Model):
+    login = models.CharField('login', max_length=50)
+    password = models.CharField('password', max_length=50)
+    email = models.EmailField('email')
+    registration_date = models.DateTimeField('registration_date')
+    banned = models.BooleanField('banned')
+    mod = models.BooleanField('mod')
+
